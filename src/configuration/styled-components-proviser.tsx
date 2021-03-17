11c871980baf23843d57769263e21649bc9d48fb
Diego Marcelino <diego@smartbr.com>
2021-03-17 12:34:43 -0300

    configuração styledcomponents ter acesso ao theme do material via props

---
+++ src/configuration/styled-components-proviser.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import { theme } from './material-provider';
+
+// Acesso ao thema do material dentro do styled-components CSS-in-JS
+
+interface StyledProviderProps {
+  children: React.ReactNode
+}
+
+export const StyledProvider = ({ children }: StyledProviderProps) => (
+  <ThemeProvider theme={theme}>
+    {children}
+  </ThemeProvider>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import ReactDOM from 'react-dom';
 import App from 'components/app';
 import MaterialProvider from 'configuration/material-provider';
 import ReduxProvider from 'configuration/redux-provider';
+import { StyledProvider } from 'configuration/styled-components-proviser';
 
 ReactDOM.render(
   <React.StrictMode>
     <ReduxProvider>
       <MaterialProvider>
-        <App />
+        <StyledProvider>
+          <App />
+        </StyledProvider>
       </MaterialProvider>
     </ReduxProvider>
   </React.StrictMode>,
