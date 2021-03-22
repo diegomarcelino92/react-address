127fb171a7b489bd8fd4c3d93f35a91b3c4fa65e
Diego Marcelino <diego@smartbr.com>
2021-03-22 15:08:13 -0300

    correção app

--- src/components/app/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-import { MAX_WIDTH } from 'configurations/constants';
-
-export const Container = styled.div`
-  max-width: ${MAX_WIDTH};
-  width: 100%;
-`;
